508db77dac3d0edeae1fa0b9d66acc18426c040c
abreham gezahegn <abreham0913@gmail.com>
2019-12-10 02:35:11 +0300

    (feat) am four five...

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,25 +1,17 @@
 const jwt = require("jsonwebtoken");
 
-const authorize = (req,res,next) => {
-	const token = req.header('auth_token');
-	if(!token) return res.status(401).send("Access denied.Token is required.")
+const authorize = (req, res, next) => {
+  const token = req.header("auth_token");
+  if (!token) return res.status(401).send("Access denied.Token is required.");
 
-	try{
-		const decoded = jwt.verify(token , process.env.JWT_SECRET);
-		req.user = decoded
-		console.log(decoded)
-		next();
-	}catch(err){
-		res.status(401).send("Access denied.Token is tempered with.")
-	}
-}
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+    console.log(decoded);
+    next();
+  } catch (err) {
+    res.status(401).send("Access denied.Token is tempered with.");
+  }
+};
 
-
-const isAdmin = (req,res,next) => {
-	if(req.user.role !== "admin") return res.status(403).send("You are trying to access a classified info.")
-	next();
-}
-
-
-
-module.exports =  authorize
+module.exports = authorize;
